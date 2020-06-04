ed314b34f558198392b22f6298279cf6980fcc19
Renald William <renald_w@hotmail.com>
2020-06-03 22:14:41 -0300

    Classe Curso criada e Testada

--- src/application/Program.java
+++ src/application/Program.java
@@ -5,6 +5,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Locale;
 
+import entities.Curso;
 import entities.Disciplina;
 import entities.Pessoa;
 import entities.Professor;
@@ -25,8 +26,20 @@ public class Program {
 		System.out.println(p2.getnRegistro());
 		
 		Disciplina d1 = new Disciplina("Estrutura De Dados",45, p2);
+		Disciplina d2 = new Disciplina("Banco  De Dados",45, p2);
 		
 		System.out.println(d1);
+		
+		Curso c1 = new Curso("TADs");
+		
+		c1.inseriDisciplina(d1);
+		c1.inseriDisciplina(d2);
+		
+		System.out.println(c1);
+		
+		//c1.removeDisciplina(d1);
+		
+		//System.out.println(c1);
 	}
 
 }
---
+++ src/entities/Curso.java
@@ -0,0 +1,46 @@
+package entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Curso {
+	
+	private String nomeCurso;
+	private List<Disciplina> disc = new ArrayList<>(); 
+	
+	public Curso() {
+	}
+
+	public Curso(String nomeCurso) {
+		super();
+		this.nomeCurso = nomeCurso;
+	}
+
+	public String getNomeCurso() {
+		return nomeCurso;
+	}
+
+	public void setNomeCurso(String nomeCurso) {
+		this.nomeCurso = nomeCurso;
+	}
+
+	public List<Disciplina> getDisciplina() {
+		return disc;
+	}
+	
+	public void inseriDisciplina(Disciplina dis) {
+		disc.add(dis);
+	}
+	
+	public void removeDisciplina(Disciplina dis) {
+		disc.remove(dis);
+	}
+
+	@Override
+	public String toString() {
+		return "Curso [nomeCurso=" + nomeCurso + ", disc=" + disc + "]";
+	}
+
+	
+
+}
