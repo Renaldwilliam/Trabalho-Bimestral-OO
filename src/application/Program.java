ea68548583c6f79d5e988edec5694da0475bb999
daniel <daniel@daniel.daniel.com.br>
2020-06-14 20:53:56 -0300

    Feito os cadastros

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,76 +1,12 @@
 package application;
 
+import entities.MenuPrincipal;
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-
-import entities.AlunosCurso;
-import entities.Aula;
-import entities.Colegiado;
-import entities.Curso;
-import entities.Disciplina;
-import entities.Estudante;
-import entities.Pessoa;
-import entities.Professor;
 
 public class Program {
-
+    
 	public static void main(String[] args) throws ParseException {
-		
-		SimpleDateFormat sdf1 = new SimpleDateFormat("dd/MM/yyyy");
-		Locale.setDefault(Locale.US);
-		Date y1 = sdf1.parse("23/12/2000");
-		
-		Pessoa p1 = new Pessoa("renald", "4658898", y1);
-		System.out.println(p1.getIdade());
-		
-		Professor p2 = new Professor("Renald", "55665", y1, "45565", "Junior", 2500.00, 55.00);
-		
-		System.out.println(p2.getnRegistro());
-		
-		Disciplina d1 = new Disciplina("Estrutura De Dados",45, p2);
-		Disciplina d2 = new Disciplina("Banco  De Dados",45, p2);
-		
-		System.out.println(d1);
-		
-		Curso c1 = new Curso("TADs");
-		
-		c1.inseriDisciplina(d1);
-		c1.inseriDisciplina(d2);
-		
-		System.out.println(c1);
-		
-		//c1.removeDisciplina(d1);
-		
-		//System.out.println(c1);
-		
-		Estudante estu = new Estudante("Marcos", "5896", y1, "45865", 3, c1);
-		Estudante estu2 = new Estudante("Marcos1", "58296", y1, "445865", 3, c1);
-		System.out.println(estu);
-		
-		
-		Colegiado col1 = new Colegiado(c1);
-		
-		col1.inseriProfessor(p2);
-		
-		System.out.println(col1);
-		
-		AlunosCurso ac = new AlunosCurso(c1);
-		
-		ac.insereAluno(estu);
-		
-		System.out.println("Alunoscurso");
-		System.out.println(ac.getAlunosCurso());
-		
-		Aula aul = new Aula("25", y1, d2);
-		
-		aul.insereAluno(estu);
-		aul.insereAluno(estu2);
-		System.out.println(aul.recuperaAluno(estu.getRa()));
-		
-		System.out.println(aul.getAlunosAula());
-		
-	}
-
+            MenuPrincipal menu = new MenuPrincipal();
+            menu.start();
+        }
 }
--- src/entities/AlunosCurso.java
+++ src/entities/AlunosCurso.java
@@ -33,8 +33,8 @@ public class AlunosCurso {
 		alunosCurso.put(estu.getRa(), estu);
 	}
 	
-	public String recuperaAluno(String ra) {
-		return alunosCurso.get(ra).getNome();
+	public Estudante recuperaAluno(String ra) {
+		return alunosCurso.get(ra);
 	}
 	
 }
